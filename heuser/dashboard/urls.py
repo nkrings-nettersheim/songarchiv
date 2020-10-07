3595a0872215ecb0cc70b7bb24ffec3c55db6355
nkrings <norbert.krings@t-online.de>
2020-10-07 07:56:51 +0200

    Version 1.11

--- heuser/dashboard/urls.py
+++ heuser/dashboard/urls.py
@@ -11,4 +11,8 @@ urlpatterns = [
     path('emptychordpro/', views.empty_chordpro, name='empty_chordpro'),
     path('emptynashville/', views.empty_nashville, name='empty_nashville'),
     path('emptystandard/', views.empty_standard, name='empty_standard'),
+    path('chordpro/', views.ChordproListView.as_view(), name='chordpro'),
+    path('chords/', views.ChordsListView.as_view(), name='chords'),
+    path('nashville/', views.NashvilleListView.as_view(), name='nashville'),
+    path('standard/', views.StandardListView.as_view(), name='standard'),
 ]
--- heuser/dashboard/views.py
+++ heuser/dashboard/views.py
@@ -1,5 +1,9 @@
+from django.db.models import Q
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.views.generic.list import ListView
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 
 from songarchiv.models import Song, Album, Song_Text
 
@@ -11,7 +15,7 @@ def index(request):
     results['emptytext'] = Song_Text.objects.filter(text_text__exact='').count()
     results['emptychords'] = Song_Text.objects.filter(text_chords__exact='').count()
     results['emptychordpro'] = Song_Text.objects.filter(text_chordpro__exact='').count()
-    results['emptynashville'] = Song_Text.objects.filter(text_nashville__exact='').count()
+    results['emptynashville'] = Song_Text.objects.filter(~Q(text_chords__exact='')).filter(text_nashville__exact='').count()
     results['emptystandard'] = Song_Text.objects.filter(text_standard_german__exact='').count()
     return render(request, 'index.html', {'results': results})
 
@@ -39,7 +43,7 @@ def empty_chordpro(request):
 
 @login_required
 def empty_nashville(request):
-    song_text = Song_Text.objects.filter(text_nashville__exact='').values('id', 'song_id')
+    song_text = Song_Text.objects.filter(~Q(text_chords__exact='')).filter(text_nashville__exact='').values('id', 'song_id')
     song_text.part = "Nashville"
     return render(request, 'dashboard/empty_text.html', {'song_text': song_text})
 
@@ -49,3 +53,31 @@ def empty_standard(request):
     song_text = Song_Text.objects.filter(text_standard_german__exact='').values('id', 'song_id')
     song_text.part = "Standard German"
     return render(request, 'dashboard/empty_text.html', {'song_text': song_text})
+
+
+class ChordproListView(LoginRequiredMixin, ListView):
+    model = Song_Text
+    context_object_name = 'my_song_list'
+    queryset = Song_Text.objects.filter(text_chordpro__exact='')
+    template_name = 'dashboard/song_list.html'
+
+
+class ChordsListView(LoginRequiredMixin, ListView):
+    model = Song_Text
+    context_object_name = 'my_song_list'
+    queryset = Song_Text.objects.filter(text_chords__exact='')
+    template_name = 'dashboard/song_list.html'
+
+
+class NashvilleListView(LoginRequiredMixin, ListView):
+    model = Song_Text
+    context_object_name = 'my_song_list'
+    queryset = Song_Text.objects.filter(~Q(text_chords__exact='')).filter(text_nashville__exact='')
+    template_name = 'dashboard/song_list.html'
+
+
+class StandardListView(LoginRequiredMixin, ListView):
+    model = Song_Text
+    context_object_name = 'my_song_list'
+    queryset = Song_Text.objects.filter(text_standard_german__exact='')
+    template_name = 'dashboard/song_list.html'
