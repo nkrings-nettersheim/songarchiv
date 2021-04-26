b4bacd37fef8e23cc793bcbb7b3879de37fd1e57
Norbert Krings <norbert.krings@t-online.de>
2021-04-26 09:43:00 +0200

    Feature "Songliste" If you click on the album-cover in the album list you get get the song list with the song text

--- heuser/songarchiv/admin.py
+++ heuser/songarchiv/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Song, Song_Text, Album, Content_text
+from .models import Song, Song_Text, Album, Content_text, Album_song
+
+class Album_song_admin(admin.ModelAdmin):
+    list_display = ('album', 'song', 'position')
 
 admin.site.register(Song)
 admin.site.register(Song_Text)
 admin.site.register(Album)
 admin.site.register(Content_text)
+admin.site.register(Album_song, Album_song_admin)
