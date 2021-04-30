7a9917e12c300542f914e97aa0e86c0c476e7fb4
Norbert Krings <norbert.krings@t-online.de>
2021-04-30 11:14:00 +0200

    more Unittests around models

--- heuser/songarchiv/tests/test_models.py
+++ heuser/songarchiv/tests/test_models.py
@@ -1,29 +1,31 @@
+from datetime import date
+
+#from django.db.models import ImageFieldFile
+from django.db import models
 from django.test import TestCase
-from ..models import Album, Song, Song_Text
-
-
-class ModelsTestCase(TestCase):
-    def setUp(self):
-        self.item = Album()
-        self.item.album_title = 'Album Titel'
-        self.item.album_year = '1965-05-21'
-        self.item.cover = 'cover.png'
-        self.item.album_spotify = 'https://www.spotify.com'
-        self.item.album_amazon_selling = 'http://www.amazon.de'
-        self.item.save()
-
-    def test_album_fields(self):
-        record = Album.objects.get(pk=1)
-        self.assertEqual(record, self.item)
+from ..models import Album, Song, Song_Text, Album_song, Content_text
 
 
 class AlbumModelTest(TestCase):
     @classmethod
     def setUpTestData(cls):
         Album.objects.create(
-            album_title='Schloflos',
+            album_title='Zick es Jlöck',
             album_single='1'
         )
+
+    # check if field exists #######################################################################
+    def test_it_has_information_fields(self):
+        album = Album.objects.get(id=1)
+        self.assertIsInstance(album.album_title, str)
+        self.assertIsInstance(album.album_spotify, str)
+        self.assertIsInstance(album.album_amazon_selling, str)
+        self.assertIsInstance(album.album_single, str)
+        self.assertIsInstance(album.album_cover_image, models.fields.files.ImageFieldFile)
+
+    def test_it_has_date(self):
+        album = Album.objects.get(id=1)
+        self.assertIsInstance(album.album_year, date)
 
     # check label #################################################################################
 
@@ -78,14 +80,51 @@ class AlbumModelTest(TestCase):
         max_length = album._meta.get_field('album_single').max_length
         self.assertEqual(max_length, 20)
 
+    # check object_name ############################################################################
+    def test_object_name(self):
+        album = Album.objects.get(id=1)
+        expected_object_name = f'{album.album_title}'
+        self.assertEqual(expected_object_name, str(album))
+
 
 class SongModelTest(TestCase):
     @classmethod
     def setUpTestData(cls):
+        album =Album.objects.create(
+            album_title='Zick es Jlöck',
+            album_single='1'
+        )
         Song.objects.create(
             song_title='Als Panz',
-            song_artist='Björn Heuser'
+            song_artist='Björn Heuser',
+            album=album
         )
+
+    # check if field exists #######################################################################
+    def test_it_has_information_fields(self):
+        song = Song.objects.get(id=1)
+        self.assertIsInstance(song.song_title, str)
+        self.assertIsInstance(song.song_tag, str)
+        self.assertIsInstance(song.slug, str)
+        self.assertIsInstance(song.song_artist, str)
+        self.assertIsInstance(song.song_music, str)
+        self.assertIsInstance(song.song_lyrics, str)
+        self.assertIsInstance(song.song_publisher, str)
+        self.assertIsInstance(song.song_producer, str)
+        self.assertIsInstance(song.song_spotify_iframe, str)
+        self.assertIsInstance(song.song_spotify, str)
+        self.assertIsInstance(song.song_amazon, str)
+        self.assertIsInstance(song.song_itunes, str)
+        self.assertIsInstance(song.song_snippet, models.fields.files.FieldFile)
+        self.assertIsInstance(song.song_youtube, str)
+        self.assertIsInstance(song.song_youtube_2, str)
+        self.assertIsInstance(song.song_youtube_3, str)
+        self.assertIsInstance(song.song_amazon_sale, str)
+        self.assertIsInstance(song.song_background, str)
+
+    def test_it_has_date(self):
+        song = Song.objects.get(id=1)
+        self.assertIsInstance(song.song_year, date)
 
     # check label #################################################################################
     def test_song_title_label(self):
@@ -291,6 +330,10 @@ class SongModelTest(TestCase):
         # This will also fail if the urlconf is not defined.
         self.assertEqual(song.get_absolute_url(), '/songarchiv/song/bjoern-heuser-als-panz/')
 
+    # check foreignkey field ######################################################################
+    def test_album_foreign_key_field(self):
+        song = Song.objects.get(id=1)
+        self.assertEqual(song.album.album_title, "Zick es Jlöck")
 
 class Song_TextModelTest(TestCase):
     @classmethod
@@ -305,11 +348,25 @@ class Song_TextModelTest(TestCase):
             song=song
         )
 
+    # check if field exists #######################################################################
+    def test_it_has_information_fields(self):
+        song_text = Song_Text.objects.get(id=1)
+        self.assertIsInstance(song_text.text_text, str)
+        self.assertIsInstance(song_text.text_standard_german, str)
+        self.assertIsInstance(song_text.text_chordpro, str)
+        self.assertIsInstance(song_text.text_chords, str)
+        self.assertIsInstance(song_text.text_nashville, str)
+
     # check label #################################################################################
     def test_text_text_label(self):
         song_text = Song_Text.objects.get(id=1)
         field_label = song_text._meta.get_field('text_text').verbose_name
         self.assertEqual(field_label, 'text text')
+
+    def test_slug_label(self):
+        song_text = Song_Text.objects.get(id=1)
+        field_label = song_text._meta.get_field('slug').verbose_name
+        self.assertEqual(field_label, 'slug')
 
     def test_text_standard_german_label(self):
         song_text = Song_Text.objects.get(id=1)
@@ -338,6 +395,99 @@ class Song_TextModelTest(TestCase):
 
     # check max_length ############################################################################
     def test_slug_max_length(self):
-        song_text = Song.objects.get(id=1)
+        song_text = Song_Text.objects.get(id=1)
         max_length = song_text._meta.get_field('slug').max_length
         self.assertEqual(max_length, 70)
+
+    # check object_name ###########################################################################
+    def test_object_name(self):
+        song_text = Song_Text.objects.get(id=1)
+        expected_object_name = f'{song_text.song.song_title}'
+        self.assertEqual(expected_object_name, str(song_text))
+
+    # check get absolute url ######################################################################
+    def test_get_absolute_url(self):
+        song_text = Song_Text.objects.get(id=1)
+        # This will also fail if the urlconf is not defined.
+        self.assertEqual(song_text.get_absolute_url(), '/songarchiv/text/bjoern-heuser-als-panz/')
+
+    # check foreignkey field ######################################################################
+    def test_song_foreign_key_field(self):
+        song_text = Song_Text.objects.get(id=1)
+        self.assertEqual(song_text.song.song_title, "Als Panz")
+
+
+class Album_songModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        song = Song.objects.create(song_title='Als Panz')
+        album = Album.objects.create(album_title='Zick es Jlöck')
+        Album_song.objects.create(
+            album=album,
+            song=song,
+            position=1
+        )
+
+    # check if field exists #######################################################################
+    def test_it_has_information_fields(self):
+        album_song = Album_song.objects.get(id=1)
+        self.assertIsInstance(album_song.position, int)
+
+    # check label #################################################################################
+    def test_album_label(self):
+        album_song = Album_song.objects.get(id=1)
+        field_label = album_song._meta.get_field('album').verbose_name
+        self.assertEqual(field_label, 'album')
+
+    def test_song_label(self):
+        album_song = Album_song.objects.get(id=1)
+        field_label = album_song._meta.get_field('song').verbose_name
+        self.assertEqual(field_label, 'song')
+
+    def test_position_label(self):
+        album_song = Album_song.objects.get(id=1)
+        field_label = album_song._meta.get_field('position').verbose_name
+        self.assertEqual(field_label, 'position')
+
+    # check object_name ############################################################################
+    def test_object_name(self):
+        album_song = Album_song.objects.get(id=1)
+        expected_object_name = ("%s %s %s %s" % (album_song.album.album_title, album_song.song.song_title, album_song.song.id, album_song.position))
+        self.assertEqual(expected_object_name, str(album_song))
+
+
+class Content_textModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.content_text = Content_text.objects.create(
+            content_kurz='Dies ist der Kurztext',
+            content_lang='Dies ist der Langtext'
+        )
+    # check if field exists #######################################################################
+    def test_it_has_information_fields(self):
+        content_text = Content_text.objects.get(id=1)
+        self.assertIsInstance(content_text.content_kurz, str)
+        self.assertIsInstance(content_text.content_lang, str)
+
+    # check label #################################################################################
+    def test_content_kurz_label(self):
+        content_text = Content_text.objects.get(id=1)
+        field_label = content_text._meta.get_field('content_kurz').verbose_name
+        self.assertEqual(field_label, 'content kurz')
+
+    def test_content_lang_label(self):
+        content_text = Content_text.objects.get(id=1)
+        field_label = content_text._meta.get_field('content_lang').verbose_name
+        self.assertEqual(field_label, 'content lang')
+
+    # check max_length ############################################################################
+    def test_content_kurz_max_length(self):
+        content_text = Content_text.objects.get(id=1)
+        max_length = content_text._meta.get_field('content_kurz').max_length
+        self.assertEqual(max_length, 256)
+
+    # check object_name ############################################################################
+    def test_object_name(self):
+        content_text = Content_text.objects.get(id=1)
+        expected_object_name = f'{content_text.content_kurz}'
+        self.assertEqual(expected_object_name, str(content_text))
